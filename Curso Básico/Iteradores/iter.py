08cf124565a6f868d9d493177000344cd7c9224a
emdajhuda <emdarodriguez@gmail.com>
2019-06-18 23:44:19 -0500

    Añadí programas de iteraciones y uso de listas :memo:.

---
+++ Curso Básico/Iteradores/iter.py
@@ -0,0 +1,6 @@
+list=[4,3,8,9]
+iter_1=iter(list)
+print(next(iter_1))
+print(next(iter_1))
+print(next(iter_1))
+print(next(iter_1))
---
+++ Curso Básico/Iteradores/lista.py
@@ -0,0 +1,4 @@
+list=[1,2,3,4,-2,5]
+list2=[n for n in list if n>0]
+print(list)
+print(list2)
